b49a927017f2795299fa4060578b8492e761fff9
Salman <salmanahmed404@gmail.com>
2020-07-29 12:40:10 +0530

    Modify: Dummy data gen script to take command line arguments

--- project/generate_dummy_data.py
+++ project/generate_dummy_data.py
@@ -1,20 +1,20 @@
 import csv
+import sys
 
 from faker import Faker
 
-RECORDS = 10000
 FIELD_NAMES = ['name', 'address', 'discipline', 'cgpa', 'date_recorded']
 
-def create_csv():
+def create_csv(records):
     """
     Function which creates a csv file with dummy data
     """
     fake = Faker()
-    with open('../data/dummy_med.csv', 'w', newline='') as dummy_csv:
+    with open('../data/dummy.csv', 'w', newline='') as dummy_csv:
         
         csvwriter = csv.DictWriter(dummy_csv, fieldnames=FIELD_NAMES)
         csvwriter.writeheader()
-        for i in range(RECORDS):
+        for i in range(records):
             csvwriter.writerow({
                 'name': fake.name(),
                 'address': fake.street_address(),
@@ -29,4 +29,7 @@ def create_csv():
 
 
 if __name__ == '__main__':
-    create_csv()
+    if (len(sys.argv) > 1):
+        create_csv(int(sys.argv[1]))
+    else:
+        create_csv(20000)
