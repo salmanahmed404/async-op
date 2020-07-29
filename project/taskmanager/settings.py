0edbb258258320eb0cbb77188a423e9e7c9fe851
Salman <salmanahmed404@gmail.com>
2020-07-29 12:18:58 +0530

    Add: API documentation

--- project/taskmanager/settings.py
+++ project/taskmanager/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS' : "rest_framework.schemas.coreapi.AutoSchema"
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
